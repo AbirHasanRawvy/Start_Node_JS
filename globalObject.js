072f81283a74b90bcaf495016233f0da0ee1682e
Abir Hasan <abirhasanrawvy@gmail.com>
2026-03-21 00:29:02 +0600

    GlobalObject.

--- globalObject.js
+++ globalObject.js
@@ -1,1 +1,24 @@
 //Global Object of NodeJs;
+console.log(__dirname);//which my folder situated;
+
+console.log(__filename);//identify the file name;
+
+//set timeout and set Interval;
+
+setTimeout(()=>{
+console.log("2 Sec Later");
+},2000);
+
+// setInterval(()=>{
+//     console.log(new Date());
+// }, 1000);
+
+//Process;
+console.log(process.platform);
+
+console.log(process.argv);
+
+//Global;
+globalThis.myName = "ALEX";
+
+console.log(myName);
