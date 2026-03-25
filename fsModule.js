2ddf4aa94ecf334e4e698d40c2c10aeaaf0f2283
Abir Hasan <abirhasanrawvy@gmail.com>
2026-03-26 01:04:19 +0600

    fsModule

---
+++ fsModule.js
@@ -0,0 +1,22 @@
+///
+const fs = require('fs');
+
+//create a new file
+fs.writeFileSync('hello.txt', 'Hello Node Js');
+
+//file read;
+let result = fs.readFileSync('hello.txt', 'utf-8');
+
+console.log(result);
+
+//write down something into the file;
+fs.appendFileSync('hello.txt', 'This is new text')
+
+//file delete
+fs.unlinkSync('hello.txt')
+
+//create folder
+fs.mkdirSync('newFolder')
+
+//folder delete
+fs.rmdirSync('newFolder');
