58664f37897ba68fb82293585acee04795df3a12
Abir Hasan <abirhasanrawvy@gmail.com>
2026-03-30 01:18:35 +0600

    FsModule.

--- fsModule.js
+++ fsModule.js
@@ -1,19 +1,20 @@
 ///
-const fs = require('fs');
+
+const fs = require("fs");
 
 //create a new file
-fs.writeFileSync('hello.txt', 'Hello Node Js');
+// fs.writeFileSync('hello.txt', 'Hello Node Js');
 
 //file read;
 // let result = fs.readFileSync('hello.txt', 'utf-8');
 
-console.log(result);
+// console.log(result);
 
 //write down something into the file;
-fs.appendFileSync('hello.txt', 'This is new text')
+// fs.appendFileSync('hello.txt', 'This is new text')
 
 //file delete
-fs.unlinkSync('hello.txt')
+// fs.unlinkSync('hello.txt')
 
 //create folder
 // fs.mkdirSync('newFolder')
@@ -22,10 +23,43 @@ fs.unlinkSync('hello.txt')
 // fs.rmdirSync('newFolder');
 
 //Asyncronous Mode;
-fs.readFile("hello1.txt", 'utf8', (err, data)=>{
+// fs.readFile("hello1.txt", "utf8", (err, data) => {
+//     if (err){
+//         console.log(err);
+//     }else{
+//         console.log(data);
+//     }
+// });
+
+// fs.appendFile('hello.txt', "\n This is the new text", (err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("New File Added");
+//     }
+// });
+
+// fs.unlink('hello.txt', (err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("File delete successfully");
+//     }
+// })
+
+//create folder
+fs.mkdir("NewFolder", (err) => {
     if(err){
-        console.log(err);
-    }else{
-        console.log(data);
-    }
+         console.log(err);
+     }else{
+         console.log("New folder created");
+     }
+});
+
+fs.rmdir("NewFolder", (err) =>{
+    if(err){
+         console.log(err);
+     }else{
+         console.log("Remove Folder");
+     }
 });
