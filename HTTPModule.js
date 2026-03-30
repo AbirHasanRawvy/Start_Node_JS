6b0e53e4041ccba486d326038d2ba64ab204e6d2
Abir Hasan <abirhasanrawvy@gmail.com>
2026-03-31 00:49:46 +0600

    HTTP Module.

--- HTTPModule.js
+++ HTTPModule.js
@@ -6,12 +6,17 @@ const http = require('http');
 const server = http.createServer((req, res)=>{
     //content set
     //Third:
-    res.writeHead(200,{"content-type": "text/plain"})
+    res.writeHead(200,{"content-type": "text/html"});
 
     // Fourth
-    res.write("Hello this is our node js server");
+    res.write('<h2>Hello this is our node js server!</h2>');
 
     //Fifth: server end;
     res.end();
 
-})
+});
+
+//6 step: server listen by port;
+server.listen(5000, () => {
+    console.log("Server is running");
+});
