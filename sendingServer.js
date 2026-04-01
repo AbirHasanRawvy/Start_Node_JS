19c7db67be3784d18191d52743333ef94dccba83
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-01 22:25:58 +0600

    Sending To Server.

--- sendingServer.js
+++ sendingServer.js
@@ -1,19 +1,34 @@
 ///
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    if(req.url === "/"){
-        fs.readFile('index.html', (err, data)=>{
-           if(err){
-            res.writeHead(5000, {'Content-Type': 'text/plain'})
-            res.end("Error loading html file")
-           }else{
-            res.writeHead(5000, { 'Content-Type': 'text/hatm'})
-            res.end(data);
-           }
-        });
+    // if(req.url === "/"){
+    //     fs.readFile('index.html', (err, data)=>{
+    //        if(err){
+    //         res.writeHead(5000, {'Content-Type': 'text/plain'})
+    //         res.end("Error loading html file")
+    //        }else{
+    //         res.writeHead(5000, { 'Content-Type': 'text/hatm'})
+    //         res.end(data);
+    //        }
+    //     });
+    // } 
+
+    // With JSON;
+    if(req.url === "/api") {
+        let user = {
+            name: "Hasib",
+            age: 25,
+            profession: "Developer",
+        };
+
+     res.writeHead(200, {'Content-Type': "application/json"});
+     res.end(JSON.stringify(user));   
     }
+
+
+
 });
 
 server.listen(5000, ()=>{
