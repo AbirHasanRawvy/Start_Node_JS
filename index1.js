a92195613e7ffe3145a2832f618054b6385fe663
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-09 21:42:17 +0600

    Index1.js

---
+++ index1.js
@@ -0,0 +1,7 @@
+///node is javascript run time invironment;
+console.log("Hello World!");
+
+///Why node JS: 
+//create web server,  API(Aplication Programable Interface), File, CLI(Command line interface),
+//connect with database, asynchronous, non-blocking IO, event loop linten;
+  
