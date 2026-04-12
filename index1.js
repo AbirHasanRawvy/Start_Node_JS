1bd3822e0fc4f0918d4b7242b17dda2a1b103a93
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-13 00:23:13 +0600

    Add files via upload

--- index1.js
+++ index1.js
@@ -21,23 +21,48 @@ console.error("New Error");
 //
 console.warn("New Error");
 
-//
-const path = require("path");
+// //
+// const path = require("path");
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
 
-//
-const fs = require("fs");
-// const { isUtf8 } = require("buffer");
+// //
+// const fs = require("fs");
+// // const { isUtf8 } = require("buffer");
 
-fs.readFile("note.txt", "utf8", (err, data) => {
+// fs.readFile("note.txt", "utf8", (err, data) => {
 
-    if (err) console.log(err);
-    console.log(data);
+//     if (err) console.log(err);
+//     console.log(data);
+// });
+
+// fs.writeFile("npte.txt", "I am also a javaScript developer.", (err) => {
+//     if (err) console.log(err);
+//     console.log("File is written");
+// });
+
+///HTTP;
+const http = require("http");//This is a module from node js which is talk with broweser;
+
+const server = http.createServer((_, res) => {
+    res.writeHead(200, { "Content-Type" : "application/json"}); //this is a header and its detect the type;
+    res.end(JSON.stringify({ name: "Abrar", course: "MERN Stack" }));
 });
 
-fs.writeFile("npte.txt", "I am also a javaScript developer.", (err) => {
-    if (err) console.log(err);
-    console.log("File is written");
-});
+// server.listen(5000, () => console.log("Server running on 5000 port"));
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === "/") {
+//       res.writeHead(200, { "Content-Type" : "text/plain"});
+//       res.end("Home Page");
+//     }else if(req.url === "/about"){
+//       res.writeHead(200, { "Content-Type" : "text/plain"});
+//       res.end("About Page"); 
+//     }else{
+//       res.writeHead(404, { "Content-Type" : "text/plain"});
+//       res.end("404 Page not found");
+//     }
+// });
+
+server.listen(3000, () => console.log("Server running on 3000 port"));
