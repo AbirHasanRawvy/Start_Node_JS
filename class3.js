8a42004aa6d08d44aeeb802b1f116e2cc71f5629
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-19 03:02:58 +0600

    OOP

---
+++ class3.js
@@ -0,0 +1,5 @@
+///OOP
+///1)Encapsulation, 2)Inheritence, 3)Polymorphijom and 4)Abstraction.
+
+//1)Encapsulation: under the capsole have some data and manupulate the data its a encapsulation;
+//2)Inheritence:
