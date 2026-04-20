d2f363c7568b464a7fc9a2d056eff14b9dd21edf
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-20 22:28:04 +0600

    OOP.

--- class3.js
+++ class3.js
@@ -35,7 +35,7 @@ console.log(p instanceof Person);
 console.log(p);
 
 console.log("===========================================");
-//Another same thing;
+//Another same thing but here method and data both writedown so its also encapsulation;
 class Car {
     constructor(brand, model){
         this.brand = brand;
@@ -51,3 +51,48 @@ const c1 = new Car("Honda", "Civic");
 
 console.log(c1.info());
 
+console.log("===========================================");
+
+//Private and public;
+
+class BankAccount {
+    #balance = 0;//with using hash its showing private;
+    constructor(owner){
+        this.owner = owner;
+    }
+    //seter function;
+    deposit(amount) {
+        this.#balance += amount;
+    }
+    //geter function;
+    get balance() {
+        return this.#balance;
+    }
+}
+
+const abirAcc = new BankAccount("Abir");
+abirAcc.deposit(500);
+
+// console.log(abirAcc.#balance);
+
+console.log("===========================================");
+
+///Constructor and Destructor;
+//Destructor: Outside work is done and autometically data will be deleted;
+
+class Rectangle {
+    constructor(width, height){
+        if (width <= 0 || height <= 0) throw new Error("Invalid Size");
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width = this.height;
+    }
+}
+
+const r1 = new Rectangle(4, 5);
+console.log(r1.area());
+console.log("===========================================");
+//Solid principle;
